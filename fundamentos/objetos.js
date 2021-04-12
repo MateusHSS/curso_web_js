1c26a7b00e138a4ddc61d7f8817738b68713a0aa
Mateus Henrique <mateus55henrique@hotmail.com>
2021-04-12 14:30:56 -0300

    Objetos

---
+++ fundamentos/objetos.js
@@ -0,0 +1,14 @@
+const obj = {}
+obj.nome = 'Celular'
+obj.preco = 5000.99
+
+obj['desconto'] = 0.4
+
+console.log(obj)
+
+const obj2 = {
+    nome : 'Camisa',
+    preco : 50.9,
+}
+
+console.log(obj2)
