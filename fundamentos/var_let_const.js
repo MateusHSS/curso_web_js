98ffc250a0b570d1d6f0a363ab21865a8bf4e789
Mateus Henrique <mateus55henrique@hotmail.com>
2021-04-12 15:50:07 -0300

    Propriedades de var, let e const

--- fundamentos/var_let_const.js
+++ fundamentos/var_let_const.js
@@ -1,4 +1,5 @@
 /*
   * var = visivel em todo o escopo do programa, EXCETO quando declarados dentro de funcoes
   * let = visivel apenas dentro do escopo no qual ele foi declarado
+  * const = o mesmo que o let, porem o valor nao pode ser mudado ou atribuido novamente
 */
