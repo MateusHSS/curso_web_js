5aef2d285be8a1e0798a07b0bcee03f583d4cbe7
Mateus Henrique <mateus55henrique@hotmail.com>
2021-04-12 14:19:13 -0300

    Template strings

---
+++ fundamentos/template_string.js
@@ -0,0 +1,15 @@
+const nome = "Mateus"
+const concat = "Ola" + nome + "!"
+
+const concat2 = `Ola ${nome}!`
+
+console.log(concat2)
+
+
+// expressoes
+
+console.log(`1+2 = ${1+2}`)
+
+const up = texto => texto.toUpperCase()
+
+console.log(`Ei... ${up('cuidado')}!`)
